9da293de36d8a12a3dc7783ed8c68871cf37a0ee
adi3man <adi3man@gmail.com>
2022-08-06 07:29:40 +0700

    first commit

---
+++ lists.py
@@ -0,0 +1,12 @@
+numbers = []
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+strings = ["Hello", "World"]
+nameList = ["Soldier Boy","Homelander","Starlight"]
+second_Name  = nameList[1]
+
+print(numbers[0],numbers[1],numbers[2])
+print(strings[0],strings[1])
+print("The second name on the names list is", second_Name)
